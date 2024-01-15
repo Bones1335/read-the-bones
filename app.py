6f055b51d0d96cebc1901f7debb549c20eb07a68
Alexander Hansen <ahansen1335@gmail.com>
2024-01-15 06:58:13 +0100

    add basic routing with the corresponding basic template to each page: blog, projects, about, and contact

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
