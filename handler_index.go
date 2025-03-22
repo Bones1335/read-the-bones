c582ead480ea6cabf6c61e9c3c9bebd7cebbbfa1
Alexander Hansen <ahansen1335@gmail.com>
2025-03-22 07:47:21 +0100

    populate home page with recent posts data

--- handler_index.go
+++ handler_index.go
@@ -3,11 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"sort"
 	"text/template"
 )
 
 func handlerGetIndex(w http.ResponseWriter, r *http.Request) {
-	text := "Hello internet! Welcome to Read the Bones :)"
+	directories, err := getDirectoryNames("content/posts")
+	if err != nil {
+		fmt.Printf("error getting directory names: %v", err)
+		return
+	}
+
+	var posts []PostMetaData
+
+	for _, dir := range directories {
+		path := fmt.Sprintf("%v/index.md", dir)
+		_, err := os.Open(path)
+		if os.IsNotExist(err) {
+			continue
+		}
+
+		metaData, err := parseMarkdown(path)
+		if err != nil {
+			fmt.Printf("error parsing markdown data: %v", err)
+			return
+		}
+		posts = append(posts, metaData)
+	}
+
+	sort.Slice(posts, func(i, j int) bool {
+		return posts[i].Date.After(posts[j].Date)
+	})
 
 	temp, err := template.ParseFiles("templates/layout.html", "templates/index.html")
 	if err != nil {
@@ -15,7 +42,7 @@ func handlerGetIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = temp.Execute(w, text)
+	err = temp.Execute(w, posts)
 	if err != nil {
 		fmt.Printf("problem executing template data: %v", err)
 		return
